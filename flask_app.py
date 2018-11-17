96d75fee7b4fd69fa67b98a020a9cfa560810507
Zachary Koszegi-Faulkner <ZachKF@M-C02VJ440G8WN.local>
2018-11-17 14:24:31 -0500

    Updating locations

--- flask_app.py
+++ flask_app.py
@@ -9,23 +9,25 @@ api = Api(app)
 ### GENERAL ###
 bigFormat = "%Y-%m-%d-%H"
 
-clubs = {
-    "Toronto Central": {
-        "address": "test address",
-        "code": "UnplugToConnect",
-        "log": {
-            datetime.datetime.now().strftime(bigFormat): {
-                "total" : 10000,
-                "times" : 1
-            }
-        }
+locations = {
+     "Central Tech TDSB - Service Location": {
+          "club" : "Toronto Kiwanis Boys and Girls Clubs",
+          "address": "725 Bathurst Street\nToronto, ON M5S 2R5",
+          "website": "http://www.believeinkids.ca",
+          "code": "UnplugToConnect",
+          "log": {
+               datetime.datetime.now().strftime(bigFormat): {
+                    "total" : 10000,
+                    "times" : 1
+               }
+          }
     }
 }
 
 users = {
-    "John": {
-        "clubs": [
-            "Toronto Central"
+    "John Doe": {
+        "locations": [
+            "Central Tech TDSB - Service Location"
         ],
         "log": {
             datetime.datetime.now().strftime(bigFormat): {
@@ -49,59 +51,61 @@ def addTimeToLog(log, stamp, time):
 
 ### USERS ###
 
-class UserLog(Resource):
-    def get(self):
-        parser = reqparse.RequestParser()
-        parser.add_argument("name")
-        args = parser.parse_args()
-        name = args["name"]
-        if (name in users):
-            return users[name], 200
-        else:
-            "club doesn't exist!", 202
+class Users(Resource):
+     def get(self):
+          parser = reqparse.RequestParser()
+          parser.add_argument("name")
+          args = parser.parse_args()
+          name = args["name"]
+          if (name in users):
+               return users[name], 200
+          else:
+               "User doesn't exist!", 400
 
-    def post(self):
-        parser = reqparse.RequestParser()
-        parser.add_argument("time")
-        parser.add_argument("name")
-        parser.add_argument("club")
-        args = parser.parse_args()
+     def post(self):
+          parser = reqparse.RequestParser()
+          parser.add_argument("time")
+          parser.add_argument("name")
+          parser.add_argument("location")
+          args = parser.parse_args()
 
-        name = args["name"]
-        club = args["club"]
-        loggedTime = int(args["time"])
+          name = args["name"]
+          location = args["location"]
+          loggedTime = int(args["time"])
 
-        bigTime = datetime.datetime.now().strftime(bigFormat)
+          bigTime = datetime.datetime.now().strftime(bigFormat)
 
-        if (club not in clubs):
-            clubs[club] = {
-                "latitude": 0,
-                "longitude": 0,
-                "log": {}
-            }
+          if (location not in locations):
+               locations[location] = {
+                    "latitude": 0,
+                    "longitude": 0,
+                    "log": {}
+               }
 
-        addTimeToLog(clubs[club]["log"], bigTime, loggedTime)
+          addTimeToLog(locations[location]["log"], bigTime, loggedTime)
 
-        if (name not in users):
-            users[name] = {
-                "clubs": [
-                    club
-                ],
-                "log": {}
-            }
+          if (name not in users):
+               users[name] = {
+                    "locations": [
+                    location
+                    ],
+                    "log": {}
+               }
 
-        return "Time logged!", 200
+          addTimeToLog(users[name]["log"], bigTime, loggedTime)
+
+          return "Time logged!", 200
 
 ### PETS ###
 
 def calculatePetHealth(user):
-    return {
-        "mood": 0,
-        "temperment": 0
-    }
+     return {
+          "mood": 0,
+          "temperment": 0
+     }
 
-class PetStatus(Resource):
-    def get(self):
+class Pets(Resource):
+     def get(self):
           parser = reqparse.RequestParser()
           parser.add_argument("name")
           args = parser.parse_args()
@@ -114,31 +118,31 @@ class PetStatus(Resource):
                return "user doesn't exist!", 202
 
 ### CLUBS ###
-class ClubLog(Resource):
+class Locations(Resource):
      def get(self):
           parser = reqparse.RequestParser()
-          parser.add_argument("club")
+          parser.add_argument("location")
           args = parser.parse_args()
-          club = args["club"]
-          if (club in clubs):
-               return clubs[club], 200
+          location = args["location"]
+          if (location in locations):
+               return locations[location], 200
           else:
-               "club doesn't exist!", 202
+               "Location doesn't exist!", 202
 
      def post(self):
           parser = reqparse.RequestParser()
           parser.add_argument("code")
           args = parser.parse_args()
-          code = args["code"]
-          for club in clubs:
-               if (clubs[club]["code"] == code):
-                    return club, 200
+          code = args["location"]
+          for location in locations:
+               if (locations[location]["code"] == code):
+                    return locations[location], 200
           return "Code invalid!", 202
 
 ### FLASK SETUP ###
-api.add_resource(PetStatus, "/pet/status")
-api.add_resource(UserLog, "/log/user")
-api.add_resource(ClubLog, "/log/club")
+api.add_resource(Pets, "/pets")
+api.add_resource(Users, "/users")
+api.add_resource(Locations, "/locations")
 
 if __name__ == '__main__':
      app.debug = True
