5b9284b8ebcdf03c332235258facd74795c16635
Zachary Koszegi-Faulkner <ZachKF@M-C02VJ440G8WN.local>
2018-11-17 21:01:15 -0500

    Increasing max on off times to 10

--- flask_app.py
+++ flask_app.py
@@ -12,7 +12,7 @@ api = Api(app)
 bigFormat = "%Y-%m-%d-%H-%M"
 maxTimeUnits = 5
 maxTimeRecord = 5 * 60 * 1000#1 * 60 * 60 * 24
-maxOnOffTimes = 5 #4 * 24
+maxOnOffTimes = 10 #4 * 24
 
 locations = {
      "Central Tech TDSB - Service Location": {
