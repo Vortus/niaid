792d06a94ed06b3874cc01b35ed11fe7639554bd
Zachary Koszegi-Faulkner <ZachKF@M-C02VJ440G8WN.local>
2018-11-18 09:38:31 -0500

    Cors still

--- flask_app.py
+++ flask_app.py
@@ -8,7 +8,7 @@ import json
 
 app = Flask(__name__)
 api = Api(app)
-CORS(app, resources={r"/*": {"origins": "localhost:8080"}})
+CORS(app, resources={r"/*": {"origins": "http://localhost:8080"}})
 
 ### GENERAL ###
 bigFormat = "%Y-%m-%d-%H-%M"
